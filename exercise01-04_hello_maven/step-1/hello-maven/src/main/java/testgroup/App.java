4fb0831b57742dd8fef9ac69ba1f77407aaa6532
sakamoto <sakamoto@securesky-tech.com>
2017-12-20 20:56:43 +0900

    add exercise01-04 sample code

---
+++ exercise01-04_hello_maven/step-1/hello-maven/src/main/java/testgroup/App.java
@@ -0,0 +1,13 @@
+package testgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
