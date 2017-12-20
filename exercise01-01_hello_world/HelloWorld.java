f1c40f3fdba1d683b8b5deb9f40391a0cb791ca8
sakamoto <sakamoto@securesky-tech.com>
2017-12-20 17:56:51 +0900

    WIP

---
+++ exercise01-01_hello_world/HelloWorld.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // コンソール出力
+        System.out.println("Hello, World");
+        // GUIメッセージボックス出力
+        JOptionPane.showMessageDialog(null, "Hello, World");
+    }
+}
